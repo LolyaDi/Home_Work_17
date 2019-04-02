994ae96b49955101e3d6927917010fc05b81baa8
LolyaDi <you@example.com>
2019-04-02 22:48:49 +0600

    StarWarsApi Home Work

---
+++ DelegatesHomeWork/Program.cs
@@ -0,0 +1,84 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Xml.Serialization;
+
+namespace DelegatesHomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.Write("Введите идентификатор персонажа: ");
+                string identifierCharacter = Console.ReadLine();
+                string url = $"https://swapi.co/api/people/{identifierCharacter}/";
+
+                List<StarWarsCharacters> characters = new List<StarWarsCharacters>();
+
+                XmlSerializer formatter = new XmlSerializer(typeof(List<StarWarsCharacters>));
+                bool check = false;
+
+                try
+                {
+                    using (FileStream fileStream = new FileStream("StarWarsCharacters.xml", FileMode.OpenOrCreate))
+                    {
+                        characters = (List<StarWarsCharacters>)formatter.Deserialize(fileStream);
+
+                        foreach (var character in characters)
+                        {
+                            if (character.Url == url)
+                            {
+                                character.ShowCharacters();
+                                check = true;
+                                Console.WriteLine("Нажмите любую клавишу для продолжения");
+                                Console.ReadKey();
+                                break;
+                            }
+                        }
+                    }
+                    Console.WriteLine("Персонаж с таким идентификатором отсутствует в файле!");
+                    Console.WriteLine("Запрос отправлен на https://swapi.co/ для получения этого персонажа");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Персонаж с таким идентификатором отсутствует в файле!");
+                    Console.WriteLine("Запрос отправлен на https://swapi.co/ для получения этого персонажа");
+                }
+
+                if (!check)
+                {
+                    try
+                    {
+                        string json;
+                        using (WebClient client = new WebClient())
+                        {
+                            json = client.DownloadString(url);
+                        }
+
+                        var character = JsonConvert.DeserializeObject<StarWarsCharacters>(json);
+                        character.ShowCharacters();
+                        characters.Add(character);
+
+                        using (FileStream fileStream = new FileStream("StarWarsCharacters.xml", FileMode.OpenOrCreate))
+                        {
+                            formatter.Serialize(fileStream, characters);
+                        }
+                        Console.WriteLine("Нажмите любую клавишу для продолжения");
+                        Console.ReadKey();
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Персонаж с таким идентификатором отсутствует!!!");
+                        Console.WriteLine("Нажмите любую клавишу для продолжения");
+                        Console.ReadKey();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ DelegatesHomeWork/StarWarsCharacters.cs
@@ -0,0 +1,100 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace DelegatesHomeWork
+{
+    [Serializable]
+    public class StarWarsCharacters
+    {
+        [JsonProperty("Name")]
+        public string Name { get; set; }
+
+        [JsonProperty("Height")]
+        public int? Height { get; set; }
+
+        [JsonProperty("Mass")]
+        public int? Mass { get; set; }
+
+        [JsonProperty("Hair_Color")]
+        public string HairColor { get; set; }
+
+        [JsonProperty("Skin_Color")]
+        public string SkinColor { get; set; }
+
+        [JsonProperty("Eye_Color")]
+        public string EyeColor { get; set; }
+
+        [JsonProperty("Birth_Year")]
+        public string BirthYear { get; set; }
+
+        [JsonProperty("Gender")]
+        public string Gender { get; set; }
+
+        [JsonProperty("Homeworld")]
+        public string Homeworld { get; set; }
+
+        [JsonProperty("Films")]
+        public List<string> Films { get; set; }
+
+        [JsonProperty("Species")]
+        public List<string> Species { get; set; }
+
+        [JsonProperty("Vehicles")]
+        public List<string> Vehicles { get; set; }
+
+        [JsonProperty("Starships")]
+        public List<string> Starships { get; set; }
+
+        [JsonProperty("Created")]
+        public DateTime? Created { get; set; }
+
+        [JsonProperty("Edited")]
+        public DateTime? Edited { get; set; }
+
+        [JsonProperty("Url")]
+        public string Url { get; set; }
+
+        public void ShowCharacters()
+        {
+            Console.Clear();
+            Console.WriteLine($"Name - {Name}");
+            Console.WriteLine($"Height - {Height}");
+            Console.WriteLine($"Mass - {Mass}");
+            Console.WriteLine($"Hair color - {HairColor}");
+            Console.WriteLine($"Skin color - {SkinColor}");
+            Console.WriteLine($"Eye color - {EyeColor}");
+            Console.WriteLine($"Birth year - {BirthYear}");
+            Console.WriteLine($"Gender - {Gender}");
+            Console.WriteLine($"Home world - {Homeworld}");
+
+            Console.WriteLine("Films - ");
+            foreach (var films in Films)
+            {
+                Console.WriteLine(films);
+            }
+
+            Console.WriteLine("Species -");
+            foreach (var species in Species)
+            {
+                Console.WriteLine(species);
+            }
+
+            Console.WriteLine("Vehicles -");
+            foreach (var vehicles in Vehicles)
+            {
+                Console.WriteLine(vehicles);
+            }
+
+            Console.WriteLine("Starships -");
+            foreach (var starships in Starships)
+            {
+                Console.WriteLine(starships);
+            }
+
+            Console.WriteLine($"Created - {Created}");
+            Console.WriteLine($"Edited - {Edited}");
+            Console.WriteLine($"Url - {Url}");
+        }
+    }
+}
